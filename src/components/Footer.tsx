57f0ec5e3df81e2cbd2ee66a8425292c27fe8172
shohan-pherones <shohan.sub.56@gmail.com>
2025-01-12 16:42:55 +0600

    add footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,49 @@
+import { Facebook, Instagram, Twitter, Youtube } from "lucide-react";
+import Link from "next/link";
+
 const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer className="footer footer-center bg-base-200 text-base-content rounded p-10">
+      <nav className="flex items-center gap-x-4 flex-wrap justify-center">
+        <Link href="/" className="link link-hover">
+          Home
+        </Link>
+        <Link href="/events" className="link link-hover">
+          Events
+        </Link>
+        <Link href="/about" className="link link-hover">
+          About
+        </Link>
+        <Link href="/contact" className="link link-hover">
+          Contact
+        </Link>
+      </nav>
+      <nav>
+        <div className="grid grid-flow-col gap-4">
+          <Link href="/" target="_blank">
+            <Facebook />
+          </Link>
+          <Link href="/" target="_blank">
+            <Instagram />
+          </Link>
+          <Link href="/" target="_blank">
+            <Twitter />
+          </Link>
+          <Link href="/" target="_blank">
+            <Youtube />
+          </Link>
+        </div>
+      </nav>
+      <aside>
+        <p>
+          Copyright © {new Date().getFullYear()} - All right reserved by{" "}
+          <Link href="/" className="link link-hover">
+            Event Ease.
+          </Link>
+        </p>
+      </aside>
+    </footer>
+  );
 };
 
 export default Footer;
