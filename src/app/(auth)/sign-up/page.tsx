4bb0b64b131d70a2f673037b8c46d9c77e6f89d6
shohan-pherones <shohan.sub.56@gmail.com>
2025-01-12 21:10:48 +0600

    add sign up page

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,5 +1,158 @@
+"use client";
+
+import Processing from "@/components/Processing";
+import { useRegistration } from "@/hooks/api-requests/useRegistration";
+import useAuth from "@/hooks/useAuth";
+import { IRegistration } from "@/interfaces";
+import { registerSchema } from "@/schemas";
+import { zodResolver } from "@hookform/resolvers/zod";
+import axios from "axios";
+import { Eye, EyeClosed } from "lucide-react";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import toast from "react-hot-toast";
+
 const SignUpPage = () => {
-  return <main>SignUpPage</main>;
+  const [shouldPasswordVisible, setShouldPasswordVisible] =
+    useState<boolean>(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IRegistration>({
+    resolver: zodResolver(registerSchema),
+  });
+  const { mutate, isLoading } = useRegistration();
+  const router = useRouter();
+  const { saveCredentialsDispatcher } = useAuth();
+  const searchParams = useSearchParams();
+  const redirectPath = searchParams.get("redirect");
+
+  const onSubmit = (data: IRegistration) => {
+    mutate(data, {
+      onSuccess: (response) => {
+        saveCredentialsDispatcher(response);
+        setTimeout(() => {
+          router.push(redirectPath || "/");
+        }, 100);
+      },
+      onError: (err) => {
+        if (axios.isAxiosError(err) && err.response) {
+          toast.error(err.response.data?.message || "An error occurred");
+        } else {
+          toast.error(err.message || "An unexpected error occurred");
+        }
+      },
+    });
+  };
+
+  return (
+    <main>
+      <section className="min-h-[calc(100vh-5rem)] container mx-auto max-w-2xl py-10 md:py-20">
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex flex-col gap-2 justify-center"
+        >
+          <h3 className="text-2xl md:text-3xl font-bold">Create an account</h3>
+          <p className="text-sm opacity-50">
+            Create your account by providing the details below. Enjoy faster
+            event creation, seamless registration, and access to exclusive perks
+            once you sign up!
+          </p>
+          <label htmlFor="name" className="form-control w-full">
+            <div className="label">
+              <span className="label-text">Fullname</span>
+            </div>
+            <input
+              {...register("name")}
+              type="text"
+              id="name"
+              placeholder="Sarah Parker"
+              className="input input-bordered w-full"
+            />
+            {errors.name && (
+              <div className="label">
+                <span className="label-text-alt text-rose-500">
+                  {errors.name.message}
+                </span>
+              </div>
+            )}
+          </label>
+          <label htmlFor="email" className="form-control w-full">
+            <div className="label">
+              <span className="label-text">Email address</span>
+            </div>
+            <input
+              {...register("email")}
+              type="email"
+              id="email"
+              placeholder="sarah@example.com"
+              className="input input-bordered w-full"
+            />
+            {errors.email && (
+              <div className="label">
+                <span className="label-text-alt text-rose-500">
+                  {errors.email.message}
+                </span>
+              </div>
+            )}
+          </label>
+          <label htmlFor="password" className="form-control w-full">
+            <div className="label">
+              <span className="label-text">Password</span>
+            </div>
+            <div className="relative">
+              <input
+                {...register("password")}
+                type={shouldPasswordVisible ? "text" : "password"}
+                id="password"
+                placeholder="Type a strong password"
+                className="input input-bordered w-full"
+              />
+              <button
+                onClick={() => setShouldPasswordVisible(!shouldPasswordVisible)}
+                type="button"
+                className="absolute top-1/2 -translate-y-1/2 right-2"
+              >
+                {!shouldPasswordVisible ? <EyeClosed /> : <Eye />}
+              </button>
+            </div>
+            {errors.password && (
+              <div className="label">
+                <span className="label-text-alt text-rose-500">
+                  {errors.password.message}
+                </span>
+              </div>
+            )}
+          </label>
+          <button
+            disabled={isLoading}
+            type="submit"
+            className="mt-3 btn btn-primary"
+          >
+            {isLoading ? <Processing /> : "Submit"}
+          </button>
+          <p className="mt-1">
+            Already have an account?{" "}
+            <button
+              type="button"
+              onClick={() =>
+                router.push(
+                  redirectPath
+                    ? `/sign-in?redirect=${redirectPath}`
+                    : "/sign-in"
+                )
+              }
+              className="link link-hover font-bold"
+            >
+              Login
+            </button>
+          </p>
+        </form>
+      </section>
+    </main>
+  );
 };
 
 export default SignUpPage;
---
+++ src/hooks/api-requests/useRegistration.ts
@@ -0,0 +1,20 @@
+import { IAuthResponse, IRegistration } from "@/interfaces";
+import { axiosInstance } from "@/lib/axiosInstance";
+import { useMutation } from "react-query";
+import { API_BASE_URL } from "../../../constants";
+
+export const useRegistration = () => {
+  const createRegistrationRequestHandler = async (
+    registrationData: IRegistration
+  ): Promise<IAuthResponse> => {
+    const res = await axiosInstance.post(
+      `${API_BASE_URL}/users/auth/register`,
+      registrationData
+    );
+    return res.data;
+  };
+
+  return useMutation<IAuthResponse, Error, IRegistration>(
+    createRegistrationRequestHandler
+  );
+};
